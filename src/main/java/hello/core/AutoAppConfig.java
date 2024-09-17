ba1be6838bf87f37c0dc712bc8efd5150c6dd45a
hye <eunhyekim811@gmail.com>
2024-09-17 18:33:27 +0900

    section 7 finish

---
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -0,0 +1,24 @@
+package hello.core;
+
+import hello.core.member.Member;
+import hello.core.member.MemberRepository;
+import hello.core.member.MemoryMemberRepository;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+
+@Configuration
+@ComponentScan(  //@Component가 붙은 클래스를 스캔해 스프링 빈으로 등록
+//        basePackages = "hello.core.member", //탐색 시작 위치
+//        basePackageClasses = AutoAppConfig.class
+        excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
+)
+public class AutoAppConfig {
+
+//    @Bean(name = "memoryMemberRepository")
+//    public MemberRepository memberRepository(){
+//        return new MemoryMemberRepository();
+//    }
+//    --> overriding bean definition(수동 빈 등록이 우선권을 가짐)
+}
